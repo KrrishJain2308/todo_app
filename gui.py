cb3743550e9598750af93c9c8e6fcf23431a10ea
krrishjain <krrishjain2308@gmail.com>
2025-06-02 13:34:48 +0530

    optimize and add theme

--- gui.py
+++ gui.py
@@ -2,16 +2,16 @@ import functions
 import FreeSimpleGUI as sg
 import time
 
-
+sg.theme('Black')
 clock = sg.Text("",key="clock")
 label = sg.Text("Type in a To-do")
 input_box = sg.InputText(tooltip="Enter To-do", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2,image_source="add.png",key="Add",mouseover_colors="LightBlue2",tooltip="Add Todo")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+edit_button = sg.Button("Edit",mouseover_colors="LightBlue2")
+complete_button = sg.Button(size=2,image_source="complete.png",key="Complete",mouseover_colors="LightBlue2",tooltip="Complete Todo")
+exit_button = sg.Button("Exit",mouseover_colors="LightBlue2")
 window = sg.Window('TodoNest',
                    layout=[[clock],
                            [label], [input_box, add_button],
@@ -29,6 +29,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case "Edit":
             try:
                 todo_to_edit = values["todos"][0]
